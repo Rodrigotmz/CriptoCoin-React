abaf7d207e04876ca28a46a5b0d1c775778263cc
RodrigoMartinez <rodrigomartinezdev@outlook.com>
2025-05-29 22:45:06 -0600

    Se crea la solicitud de apis con axios. Se agrega el paquete de axios para realizar solicitudes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import reactLogo from './assets/react.svg';
 import './App.css'
+import { useEffect, useState } from 'react'; 
+import axios from 'axios';
 
-function App() {
+const reqAxios = {
+  baseURL: 'https://rest.coincap.io/v3/assets',
+  method: 'get',
+  params: {
+    apiKey: ""
+  },
+  headers: { 'Content-Type': 'application/json' },
+  responseType: 'json'
+}
+
+
+
+const App = () => {
+
+    const [crypts, setCrypts] = useState()
+
+  useEffect(() => {
+    const request = async () =>{
+      try {
+        const req = await axios(reqAxios);
+        setCrypts(req.data.data);
+      }
+      catch (ex) {
+        console.log(ex)
+      }
+    }
+    request();
+  }, [])
+
+  if(!crypts) return <><span className='loader'>Cargando...</span></>
+
   return (
-    <h1>Hola mundo</h1>
+    <section>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {crypts.map(({ name, priceUsd }) => (
+          <li> Nombre: {name} Price Ud: { Number(priceUsd).toFixed(2) }</li>
+        )) }
+      </ol>
+    </section>
   )
 }
 
-export default App
+export default App;
